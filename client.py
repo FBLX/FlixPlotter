25eca0985bc5c825133c3c88d91a1ad47cb511f8
FlixBeee <behnkefelix+github@gmail.com>
2019-12-13 18:19:59 +0100

    added circle (k) function

--- client.py
+++ client.py
@@ -4,15 +4,19 @@ import time
 import random
 import numpy as np
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 5)
+import matplotlib.pyplot as plt
 
-baseLength = 800
-height = 1000
-stepsPerRev = 200
-pi = np.pi
-axDiam = 14
+ser = serial.Serial('/dev/ttyACM1', 9600, timeout = 5)
 
-ds = pi*axDiam/stepsPerRev
+baseLength = 800 # distance between rotors
+height = 1000 # canvas height
+stepsPerRev = 200 # steps per revolution
+pi = np.pi # PI
+axDiam = 14 # axis diameter of stepper motors
+
+ds = pi*axDiam/stepsPerRev # step size 
+
+res = 8 # drawing resolution in mm 
 
 
 def close():
@@ -26,8 +30,8 @@ def calcLength(x,y):
 
 
 
-def send_s(_l, _r):
-    text = 's -l ' + str(_l) + ' -r ' + str(r)
+def send_s(_l, _r, _s):
+    text = 's -l ' + str(_l) + ' -r ' + str(r) + ' -s ' + str(_s)
     text = text.encode()
     ser.write(text)
     ser.write(b'\n')
@@ -61,7 +65,40 @@ def haus():
         l,r = calcLength(i,j)
         send_m(l,r)
 
-    send_r()
+
+def line(start, end):
+    distance = np.sqrt((abs(end[0]-start[0]))**2+(abs(end[1]-start[1]))**2)
+    points = distance / res
+    print (points)
+    x = np.linspace(start[0], end[0], points)
+    y = np.linspace(start[1], end[1], points)
+
+
+    for i,j in zip(x,y):
+        l,r = calcLength(i,j)
+        send_m(l,r)
+        time.sleep(0.01)
+
+
+def circle(center, r):
+    diam = 2*pi*r
+    points = diam / res
+    dp = np.linspace(0,2*pi, points)
+    x = np.copy(dp)
+    y = np.copy(dp)
+    x = center[0] + r*np.cos(x)
+    y = center[1] + r*np.sin(y)
+
+    
+    plt.plot(x,y)
+
+    for i,j in zip(x,y):
+        l,r = calcLength(i,j)
+        send_m(l,r)
+        time.sleep(0.01)
+
+
+
 
 while(1):
     txt = input("$ ")
@@ -75,19 +112,30 @@ while(1):
         l,r = calcLength(x,y)
         send_m(l,r)
     if txt == 'd':
-        send_d(l,r)
+        send_d(l,r,s)
     if txt == 's':
         l = int(float(input("length left: "))/ds+0.5)
         r = int(float(input("length right: "))/ds+0.5)
-        send_s(l,r)
+        s = int(input("max speed: "))
+        send_s(l,r,s)
     if txt == 'r':
         print('drawing...')
         send_r()
     if txt == 'i':
         send_i()
 
-    if txt == 'h':
+    if txt == 'n':
         haus()
+
+    if txt == 'l':
+        s = (300,400)
+        e = (600,600)
+        line(s,e)
+
+    if txt == 'k':
+        cc = (300,400)
+        rr = 100
+        circle(cc,rr)
 
     time.sleep(0.5)
     while ser.in_waiting:
